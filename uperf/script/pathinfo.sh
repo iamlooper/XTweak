0904f5509e521329b7dca8f3f6a0ae4f9e63718d
LOOPER <84635379+mightylooper@users.noreply.github.com>
2021-08-09 20:40:17 +0500

    Improved path info
    Credits: fera volt

--- uperf/script/pathinfo.sh
+++ uperf/script/pathinfo.sh
@@ -3,14 +3,13 @@
 # https://github.com/yc9559/
 # Author: Matt Yang
 
-SCRIPT_DIR="script"
-BIN_DIR="bin"
-MODULE_PATH="$(dirname $(readlink -f "$0"))"
-MODULE_PATH="${MODULE_PATH%\/$SCRIPT_DIR}"
-USER_PATH="/sdcard/yc/uperf"
+MODULE_PATH="/data/adb/modules/xtweak"
+USER_PATH="/sdcard/XTweak/uperf"
 PANEL_FILE="$USER_PATH/panel_uperf.txt"
 LOG_FILE="$USER_PATH/log_uperf_initsvc.log"
 FLAGS="$MODULE_PATH/flags"
+SCRIPT_DIR="$MODULE_PATH/script"
+BIN_DIR="$MODULE_PATH/bin"
 
 # use private busybox
 PATH="/sbin:/system/sbin:/system/xbin:/system/bin:/vendor/xbin:/vendor/bin"
