f9d7f4807867df565eb283bb0bb7d12e32ee0569
LOOPER <84635379+mightylooper@users.noreply.github.com>
2021-08-09 20:49:22 +0500

    Update vtools-powercfg.sh

--- uperf/script/vtools-powercfg.sh
+++ uperf/script/vtools-powercfg.sh
@@ -1,7 +1,4 @@
-#! /vendor/bin/sh
+#!/system/bin/sh
 # Uperf https://github.com/yc9559/uperf/
 # Author: Matt Yang
-
 # powercfg wrapper for com.omarea.vtools
-# MAKE SURE THAT THE MAGISK MODULE "Uperf" HAS BEEN INSTALLED
-
