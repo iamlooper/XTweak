9bc8d21e33408395f1990bd2f392d0c713bbb434
LOOPER <84635379+mightylooper@users.noreply.github.com>
2021-08-09 20:28:14 +0500

    Some misc fixes
    Credits: fera volt

--- uperf/run_uperf.sh
+++ uperf/run_uperf.sh
@@ -1,10 +1,10 @@
-#!/vendor/bin/sh
+#!/system/bin/sh
 # Uperf Runner
 # https://github.com/yc9559/
 # Author: Matt Yang
 # Version: 20200401
 
-BASEDIR="$(dirname $(readlink -f "$0"))"
+BASEDIR="/data/adb/modules/uperf
 SCRIPT_DIR="$BASEDIR/script"
 
 # support vtools
@@ -18,7 +18,9 @@ echo "sh $SCRIPT_DIR/powercfg_main.sh \"\$1\"" >> /data/powercfg.sh
 
 # create busybox symlinks
 BB=$BASEDIR/bin/busybox
-$BB/busybox --install -s $BB
+if [ ! -f "$BB/sh" ]; then
+ "$BB"/busybox --install -s "$BB"
+fi
 
 $BB/sh $SCRIPT_DIR/prepare.sh
 $BB/sh $SCRIPT_DIR/powercfg_once.sh
