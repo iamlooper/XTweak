bb0c3b8b0a94bf66ef7eb0886ea826f0c4834eb8
LOOPER <84635379+anylooper@users.noreply.github.com>
2021-08-14 11:04:21 +0500

    Update service.sh

--- uperf/service.sh
+++ uperf/service.sh
@@ -1,3 +1,14 @@
 #!/system/bin/sh
 BASEDIR="/data/adb/modules/uperf"
 sh $BASEDIR/initsvc_uperf.sh
+# Update XTweak scripts
+function fetch_util() {
+wget -O "$MODPATH/system/bin/x-auto" "https://raw.githubusercontent.com/anylooper/XTweak/main/uperf/script/x-auto.sh"
+wget -O "$MODPATH/system/bin/x-menu" "https://raw.githubusercontent.com/anylooper/XTweak/main/uperf/script/x-menu.sh"
+wget -O "$MODPATH/system/bin/xtweak" "https://raw.githubusercontent.com/anylooper/XTweak/main/uperf/script/xtweak.sh"
+}
+
+fetch_utils
+
+# Start XTweak script
+xtweak
