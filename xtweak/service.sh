9ae9c1fa3698a44066eb6f9c6be944867ae96dcd
LOOPER <84635379+iamlooper@users.noreply.github.com>
2021-08-27 17:51:26 +0500

    Updated

--- xtweak/service.sh
+++ xtweak/service.sh
@@ -1,34 +1,16 @@
 #!/system/bin/sh
 BASEDIR="/data/adb/modules/xtweak"
-function fetch_util() {
-wget -O "$MODPATH/system/bin/xqcom" "https://raw.githubusercontent.com/anylooper/XTweak/main/uperf/script/xqcom.sh"
-wget -O "$MODPATH/system/bin/x-auto" "https://raw.githubusercontent.com/anylooper/XTweak/main/uperf/script/x-auto.sh"
-wget -O "$MODPATH/system/bin/x-menu" "https://raw.githubusercontent.com/anylooper/XTweak/main/uperf/script/x-menu.sh"
-wget -O "$MODPATH/system/bin/xtweak" "https://raw.githubusercontent.com/anylooper/XTweak/main/uperf/script/xtweak.sh"
-}
-function wait_until_sign_in() {
-    # we doesn't have the permission to rw "/sdcard" before the user unlocks the screen
-    while [ $(getprop sys.boot_completed) -eq 1 ] | [ ! -d "/sdcard" ]
-    do
-       sleep 0.1
-    done
-    local test_file="/sdcard/.PERMISSION_TEST"
-    touch "$test_file"
-    while [ ! -f "$test_file" ]; do
-        touch "$test_file"
-        sleep 0.1
-    done
-    rm -Rf "$test_file"
-}
+# Wait for boot to finish completely
+while [ $(getprop sys.boot_completed) -ne 1 ]
+do
+       sleep 1
+done
 
-# Wait until boot completed and perm given to write
-wait_until_login
+# Sleep until some time to complete init
+sleep 30
 
 # Start uperf
 sh $BASEDIR/initsvc_uperf.sh
-
-# Updated XTweak scripts
-fetch_utils
 
 # Start qcom optimization
 xqcom
